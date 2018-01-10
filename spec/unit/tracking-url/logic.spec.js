3395397a09a18439491c7b30ed1019fb3cbb7b1c
aletheia <bianchi.luca@gmail.com>
2018-01-10 08:02:24 +0100

    Add logic and specs. refs #3

--- spec/unit/tracking-url/logic.spec.js
+++ spec/unit/tracking-url/logic.spec.js
@@ -1,27 +1,62 @@
 'use strict';
 
-const helper = require('../../helper');
+const _ = require('lodash'),
+    helper = require('../../helper'),
+    Logic = require('../../../src/tracking-url/logic.js');
 
-describe('Logic.js', () => {
-    const Logic = require('../../../src/tracking-url/logic');
-    let sut;
-
-    beforeEach(() => {
-        const logger = helper.getLoggerMock();
-        sut = new Logic(logger);
+describe('TrackingUrl Logic', function () {
+    var repositoryAdapter, sut;
+    beforeEach(function () {
+        repositoryAdapter = {
+            createTrackingUrl: function () {}
+        };
+        sut = new Logic(helper.getLoggerMock(), repositoryAdapter);
     });
 
-    it('is defined', () => {
-        expect(sut).toBeDefined();
-    });
+    describe('createTrackingUrl()', function () {
+        var principal = 'principal';
+        var uuid = 'uuid';
+        var trackingObj = {
+            uuid: uuid,
+            trackers: [
+                {
+                    email: 'test@testing.org'
+                }
+            ]
+        };
 
-    describe('.sayHello', () => {
-        it('returns an hello world message', () => {
+        it('should throw if repositoryAdapter.createTrackingUrl() throws', function () {
+            var error = new Error('error');
+            spyOn(repositoryAdapter, 'createTrackingUrl').and.returnValue(Promise.reject(error));
             return sut
-                .sayHello()
-                .then((result) => {
-                    expect(result).toBe('Hello World');
+                .createTrackingUrl(principal, trackingObj)
+                .then(helper.thenFail)
+                .catch(function (err) {
+                    expect(err).toEqual(error);
+                    expect(repositoryAdapter.createTrackingUrl).toHaveBeenCalledWith(trackingObj);
                 });
+        });
+        it('should set created and lastModified', function () {
+            spyOn(repositoryAdapter, 'createTrackingUrl').and.callFake(function (ide) {
+                return Promise.resolve(ide);
+            });
+            return sut.createTrackingUrl(principal, trackingObj).then(function (result) {
+                expect(repositoryAdapter.createTrackingUrl).toHaveBeenCalled();
+                expect(result.created).toBeDate();
+                expect(result.lastModified).toEqual(result.created);
+            });
+        });
+        it('should set trackers if needed', function () {
+            spyOn(repositoryAdapter, 'createTrackingUrl').and.callFake(function (ide) {
+                return Promise.resolve(ide);
+            });
+            return sut.createTrackingUrl(principal, trackingObj).then(function (result) {
+                expect(result.trackers).toBeDefined();
+                result.trackers.forEach(tracker => {
+                    expect(tracker.trackerId).toBeDefined();
+                    expect(_.isString(tracker.trackerId)).toBeTruthy();
+                });
+            });
         });
     });
 });
--- src/tracking-url/logic.js
+++ src/tracking-url/logic.js
@@ -1,18 +1,54 @@
 'use strict';
 
-const Promise = require('bluebird');
+var _ = require('lodash'),
+    NspError = require('../nsp-error.js'),
+    Promise = require('bluebird');
 
-const Logic = function (logger) {
+function checkExistence (entity, uuid) {
+    if (_.isNull(entity)) {
+        throw new NspError(NspError.codes.IDENTITY_NOT_FOUND, 'Identity "' + uuid + '" not found');
+    }
+}
 
-    this.sayHello = function () { // example function required by logic
-        logger.verbose('Logic/sayHello'); // injected object is called
-        return Promise.try(function () { // always wrap into a Promise.try to handle errors
-            return 'Hello World';
+function checkReadOnlyAttributes (oldEntity, newEntity, attributes) {
+    var errors = [];
+    attributes.forEach(function (attribute) {
+        if (!_.isEqual(oldEntity[attribute], newEntity[attribute])) {
+            errors.push('Cannot change read-only property: ' + attribute);
+        }
+    });
+    if (errors.length > 0) {
+        throw new NspError(NspError.codes.IDENTITY_UNPROCESSABLE, 'Cannot change read-only properties', errors);
+    }
+}
+
+function checkUpdate (oldEntity, newEntity) {
+    checkReadOnlyAttributes(oldEntity, newEntity, ['uuid', 'organizationId', 'created', 'lastModified']);
+}
+
+function Logic (logger, repositoryAdapter) {
+    this.ME = 'me';
+
+    this.createTrackingUrl = function (principal, trackingObj) {
+        logger.verbose('Logic/createTrackingUrl', { principal, trackingObj });
+        return Promise.try(function () {
+            var now = new Date();
+            trackingObj.created = now;
+            trackingObj.lastModified = now;
+            trackingObj.trackers = trackingObj.trackers.map(tracker => {
+                const trackerId = Math.random()
+                    .toString(36)
+                    .substring(2, 15);
+                return Object.assign({}, tracker, { trackerId });
+            });
+            return repositoryAdapter.createTrackingUrl(trackingObj);
+        }).then(function () {
+            console.log(trackingObj)
+            return trackingObj;
         });
     };
+}
 
-};
-
-Logic.$inject = ['logger'];
+Logic.$inject = ['logger', 'identity.repository-adapter'];
 
 module.exports = Logic;
